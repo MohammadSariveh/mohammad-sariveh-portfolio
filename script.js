2dc77f689cf1da21732cf4aaacd55aafb6a08066
Mohammad Sariveh <130409076+MohammadSariveh@users.noreply.github.com>
2026-06-01 22:54:35 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 
 const t=document.getElementById('theme-toggle');
-if(localStorage.getItem('theme')==='dark'){document.body.classList.add('dark-mode');}
-t.addEventListener('click',()=>{
-document.body.classList.toggle('dark-mode');
-localStorage.setItem('theme',document.body.classList.contains('dark-mode')?'dark':'light');
-});
-const b=document.getElementById('backToTop');
-window.addEventListener('scroll',()=>{b.style.display=window.scrollY>300?'block':'none';});
-b.addEventListener('click',()=>window.scrollTo({top:0,behavior:'smooth'}));
+t.onclick=()=>document.body.classList.toggle('dark');
+const b=document.getElementById('topBtn');
+window.addEventListener('scroll',()=>b.style.display=window.scrollY>300?'block':'none');
+b.onclick=()=>window.scrollTo({top:0,behavior:'smooth'});
